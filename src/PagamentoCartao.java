dc9c3c3faee4aeaa31c20dde54dba021375c9afc
Guilherme <guilhermebeserra2007@gmail.com>
2026-06-22 23:29:35 -0300

    ♻️ refactor: manter estrutura simples dos arquivos Java

---
+++ src/PagamentoCartao.java
@@ -0,0 +1,8 @@
+/**
+ * Classe PagamentoCartao, subclasse de Pagamento.
+ * Deve aplicar taxas de 2,5% por parcela extra acima de 3 parcelas (limite 6x).
+ */
+public class PagamentoCartao extends Pagamento {
+    // TODO: Implementar atributos específicos (parcelas) e construtores
+    // TODO: Sobrescrever calcularValorFinal()
+}
---
+++ src/PagamentoDinheiro.java
@@ -0,0 +1,8 @@
+/**
+ * Classe PagamentoDinheiro, subclasse de Pagamento.
+ * Deve aplicar 5% de desconto no valor base da consulta.
+ */
+public class PagamentoDinheiro extends Pagamento {
+    // TODO: Implementar construtores
+    // TODO: Sobrescrever calcularValorFinal() aplicando 5% de desconto
+}
