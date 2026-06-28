8ecbdcb33a7a09afef355e1647976d1c333e4cdc
Guilherme Beserra <guilhermebeserra2007@gmail.com>
2026-06-27 21:50:07 -0300

    🏗️ feat(pessoa): criar classe abstrata Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,9 +1,51 @@
-/**
- * Classe abstrata Pessoa que servirá como base para Paciente e Profissional.
- * Deve conter os atributos comuns: nome, cpf, idade, telefone e data de nascimento.
- */
 public abstract class Pessoa {
-    // TODO: Implementar atributos encapsulados (R1/R2)
-    // TODO: Implementar construtores e getters/setters
-    // TODO: Implementar método abstrato exibirResumo()
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String dataNascimento;
+
+    public Pessoa(String nome, String cpf) {
+        this(nome, cpf, "", "");
+    }
+
+    public Pessoa(String nome, String cpf, String telefone, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public abstract String exibirResumo();
 }
