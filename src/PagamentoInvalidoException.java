1e2d9a4483afc411d6317c5e9548a539e70687b8
Felipe Gabriel <felipegabrieldsd@gmail.com>
2026-06-28 01:56:14 -0300

    feat: criar PagamentoCartao com validacao de parcelas

--- src/PagamentoInvalidoException.java
+++ src/PagamentoInvalidoException.java
@@ -1,6 +1,3 @@
-/**
- * Exceção personalizada para tratar valores, parcelas ou tipos inválidos de pagamento.
- */
 public class PagamentoInvalidoException extends Exception {
     public PagamentoInvalidoException(String mensagem) {
         super(mensagem);
